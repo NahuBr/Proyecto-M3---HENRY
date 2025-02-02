e63d9750b864d222dac637d40d22da12105bb06e
NahueBr <nahuebr01@gmail.com>
2025-02-02 20:44:59 -0300

    back listo

--- back/src/config/data-source.ts
+++ back/src/config/data-source.ts
@@ -6,14 +6,16 @@ dotenv.config();
 
 export const AppDataSource = new DataSource({
     type: "postgres",
-    url: process.env.DATABASE_URL,  
-    synchronize: true,  
-    logging: false,  
-    entities: [User, Credential],
-    subscribers: [],
-    migrations: [],
-    ssl: { rejectUnauthorized: false } // ✅ Necesario para Railway
-});
+    url: process.env.DATABASE_URL,
+    entities: [__dirname + "/entities/*.ts"],
+    synchronize: true,
+    ssl: { rejectUnauthorized: false },
+    extra: {
+      statement_timeout: 10000,
+      query_timeout: 10000,
+      options: "-c password_encryption=scram-sha-256",
+    },
+  });
 
 // Repositorios para interactuar con las entidades
 export const UserModel = AppDataSource.getRepository(User);
