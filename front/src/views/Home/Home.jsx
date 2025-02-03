b10474090711fb69ffe73aad6b62e89da69e2fb9
NahueBr <nahuebr01@gmail.com>
2025-02-03 17:00:43 -0300

    fix

--- front/src/views/Home/Home.jsx
+++ front/src/views/Home/Home.jsx
@@ -1,20 +1,47 @@
-import { useContext, useEffect } from "react"
-import { useNavigate } from "react-router-dom"
-import { UserContext } from "../../context/UserContext"
+import { useContext, useEffect } from "react";
+import { useNavigate } from "react-router-dom";
+import { UserContext } from "../../context/UserContext";
 
-export const Home = ()=>{    
+export const Home = () => {    
+    const navigate = useNavigate();
+    const { user } = useContext(UserContext);
 
-    const navigate = useNavigate()
+    useEffect(() => {
+        if (!user.name) {
+            navigate("/");
+        }
+    }, [user, navigate]);
 
-    const {user} = useContext(UserContext)
+    const puntosMontos = [
+        { puntos: 100, monto: "100$" },
+        { puntos: 200, monto: "250$" },
+        { puntos: 500, monto: "650$" },
+        { puntos: 1000, monto: "1500$" },
+        { puntos: 2000, monto: "3500$" },
+        { puntos: 4000, monto: "8000$" },
+        { puntos: 10000, monto: "25000$" }
+    ];
 
-    useEffect(()=>{
-            if(!user.name){
-                navigate("/")
-            }
-        },[])
+    return (
+        <>
+            <h1>Bienvenido/a {user.name}</h1>
 
-    return(<>
-        <h1>Bienvenido/a {user.name}</h1>
-    </>)
-}
+            <table>
+                <thead>
+                    <tr>
+                        <th>Puntos</th>
+                        <th>Monto</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {puntosMontos.map((item, index) => (
+                        <tr key={index}>
+                            <td>{item.puntos}</td>
+                            <td>{item.monto}</td>
+                        </tr>
+                    ))}
+                </tbody>
+            </table>
+        </>
+    );
+};
